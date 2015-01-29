c177b3c835df15cad3b31381b4bf51af7fa3a59a
Yannick <contact@studio384.be>
2015-01-29 21:42:11 +0100

    Add build data 9926

---
+++ 9926.php
@@ -0,0 +1,194 @@
+<div class="panel panel-warning">
+	<div class="panel-heading">
+		<h3 class="panel-title">Technical Preview v10.0.9926<span class="pull-right">23.01.'15</span></h3>
+	</div>
+	<div class="panel-body">
+		<h2>What's new in the Windows 10 Technical Preview 4?</h2>
+		<p>Windows will update from version 6.4.9879 to 10.0.9926 after launching this update.</p>
+		<div class="alert alert-success">
+			<h4>Re-enabled features</h4>
+			Universal scroll is back in this build as optional feature.
+		</div>
+		<h3>Desktop</h3>
+		<ul>
+			<li>The desktop has received a new interface</li>
+			<li>Multiple annimations have been altered</li>
+			<li>Win32 apps now also use gestures</li>
+			<li>Multiple icons have been redesigned to match the new UI</li>
+			<li>The search button is replaced with a searchbar by default</li>
+			<li>You can now choose the default folder for File Explorer</li>
+			<li>Win32 window chromes are now more similar to WinRT apps</li>
+			<li>Personalisation
+				<ul>
+					<li>The window colorchoises have been changed</li>
+					<li>There are new backgrounds</li>
+				</ul>
+			</li>
+		</ul>
+		<h3>Start</h3>
+		<ul>
+			<li>Continuum is now available</li>
+			<li>The Start grid has been improved</li>
+			<li>The Start screen grid now scrolls vertical</li>
+			<li>Tiles no longer use a gradient and border</li>
+		</ul>
+		<h3>Features</h3>
+		<ul>
+			<li>Action Center
+				<ul>
+					<li>The Action Center has been revamped</li>
+					<li>You can now change quick settings in the Action Center</li>
+					<li>Notifications now can contain actions</li>
+					<li>A swype from the right now opens the Action Center</li>
+					<li>Quick settings can now contain more settings</li>
+					<li>Notifications have received a new design</li>
+				</ul>
+			</li>
+			<li>It's easier to connect to wireless audio and video devices</li>
+			<li>ALT+TAB now opens a view that's merged with the Task View</li>
+			<li>Calculator has been removed</li>
+			<li>A "Pin to quick access" options has been added to the ribbon in the File Explorer, replacing Favorites</li>
+		</ul>
+		<h3>Internet Explorer</h3>
+		<ul>
+			<li>The Edge 12 rendering engine has been updated with lots of new features
+				<ul>
+					<li>HTTP Strict Transport Security (HSTS)</li>
+					<li>HTTP Live Streaming (HLS)</li>
+					<li>Video Tracks</li>
+					<li>DOM L3 XPath</li>
+					<li>Improved ECMAScript 6 Standard support</li>
+					<li>Improved ECMAScript non-standard support</li>
+				</ul>
+			</li>
+			<li>F12 developertools improvements
+				<ul>
+					<li>New and improved Network Tools</li>
+					<li>HTML and CSS Pretty Printing support</li>
+					<li>Async Callstacks for Events and Timers</li>
+					<li>Sourcemaps for Styles and in the Memory Profiler</li>
+					<li>Find references and Go To Definitions</li>
+				</ul>
+			</li>
+			<li>More Windows Insiders will get Edge 12 as the default engine to replace Trident 7</li>
+		</ul>
+		<h3>WinRT</h3>
+		<ul>
+			<li>WinRT apps have a new border chrome</li>
+			<li>Full screen mode is now a button on the window instead of in the hamburger menu</li>
+			<li>Settings fly-out now appears within an app</li>
+			<li>The Charms bar has been removed</li>
+			<li>Apps can now run within File Explorer (ot pick a photo, Photos will load in File Explorer, for example)</li>
+		</ul>
+		<h3>Cortana</h3>
+		<ul>
+			<li>Cortana has been added</li>
+		</ul>
+		<h3>Apps</h3>
+		<ul>
+			<li>Alarms
+				<ul>
+					<li>Alarms has been redesigned</li>
+					<li>There is now a world clock function</li>
+				</ul>
+			</li>
+			<li>Calculator
+				<ul>
+					<li>Calculator has received a new design</li>
+				</ul>
+			</li>
+			<li>Camera
+				<ul>
+					<li>Camera has been revamped</li>
+				</ul>
+			</li>
+			<li>Contact Support
+				<ul>
+					<li>Contact Support is a new app</li>
+				</ul>
+			</li>
+			<li>Getting Started
+				<ul>
+					<li>Help + Tips has been renamed to Getting Started</li>
+					<li>Getting Started has been redesigned</li>
+				</ul>
+			</li>
+			<li>Insiders Hub
+				<ul>
+					<li>Insiders Hub has been redesigned</li>
+				</ul>
+			</li>
+			<li>Maps
+				<ul>
+					<li>Maps has been revamped</li>
+				</ul>
+			</li>
+			<li>OneNote
+				<ul>
+					<li>OneNote has been updated to OneNote Preview</li>
+					<li>OneNote now has a ribon design</li>
+					<li>You can now give feedback from the sidebar</li>
+					<li>You can now sync text-only for slow or mobile data connections</li>
+					<li>The radial menu has been removed</li>
+					<li>There are new mark-up options
+						<ul>
+							<li>You can now attach documents and pictures</li>
+							<li>You can now make checkbox lists</li>
+							<li>You can now add tables</li>
+							<li>Text can be highlighted with your fingers</li>
+						</ul>
+					</li>
+					<li>KB3034229 is required to see the new OneNote app</li>
+				</ul>
+			</li>
+			<li>Photos
+				<ul>
+					<li>Photos has been revamped</li>
+					<li>New Photos manage settings and features</li>
+					<li>Photos now remove duplicates automaticaly</li>
+					<li>Photos are now enhanced automaticaly</li>
+				</ul>
+			</li>
+			<li>Settings
+				<ul>
+					<li>Settings has been merged with Control Panel</li>
+					<li>Settings has been revamped completely</li>
+					<li>New options for Aero Snap have been added</li>
+					<li>When downloading a new build, Windows Update will also show a progress bar</li>
+					<li>You can now choose to enable scrolling in unactive windows</li>
+					<li>There are new options to schedule restarts</li>
+					<li>Universal scroll is now added as an option under settings</li>
+				</ul>
+			</li>
+			<li>Windows Feedback
+				<ul>
+					<li>Windows Feedback has received a new design</li>
+				</ul>
+			</li>
+			<li>Windows Store beta
+				<ul>
+					<li>Windows Store beta has been added as a new app</li>
+				</ul>
+			</li>
+			<li>Xbox
+				<ul>
+					<li>Xbox has been revamped</li>
+				</ul>
+			</li>
+		</ul>
+		<h3>Media support</h3>
+		<ul>
+			<li>System-wide support for the FLAC audio format</li>
+			<li>System-wide support for the ALAC audio format</li>
+		</ul>
+		<h3>And further</h3>
+		<ul>
+			<li>The Windows kernel has been updated from version 6.4 to 10.0</li>
+			<li>The builds are now called Windows 10 Pro Technical Preview (instead of Windows Technical Preview)</li>
+			<li>The installation interface has been altered</li>
+			<li>The preview is now available in Japanese, Russian, German, French, French (Canada), Korean, Italian, Spanish, Spanish (Latin America), Traditional Chinese, Swedish, Finnish, Turkish, Arabic, Dutch, Czech, Polish, Thai, Vietnamese (Language Interface Pack), Catalan (Language Interface Pack), and Hindi (Language Interface Pack)</li>
+			<li>Support for the Persian calendar</li>
+			<li>Updating to this build might reset some of your settings, even from official builds from Microsoft (like 7601, 9200, 9600, 9841, 9860 and 9879)</li>
+		</ul>
+	</div>
+</div>
