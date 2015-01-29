1e7dbd3b89e55824d2bee09c86efadb6a4ba98c3
Yannick <contact@studio384.be>
2015-01-29 21:41:55 +0100

    Add build data 9860

---
+++ 9860.php
@@ -0,0 +1,66 @@
+<div class="panel panel-warning">
+	<div class="panel-heading">
+		<h3 class="panel-title">Technical Preview v6.4.9860<span class="pull-right">21.10.'14</span></h3>
+	</div>
+	<div class="panel-body">
+		<h2>What's new in the Windows Technical Preview 2?</h2>
+		<p>Windows will update from version 6.4.9841 to 6.4.9860 after launching this update.</p>
+		<div class="alert alert-danger">
+			<h4>Disabled features</h4>
+			Universal scroll and the new snap modes has been disabled in this build.
+		</div>
+		<h3>Desktop</h3>
+		<ul>
+			<li>The Action Center icon has been added to the taskbar</li>
+			<li>Universal Apps now have a black titlebar instead of a colored one</li>
+			<li>Clicking the internet connection icon on the taskbar will open the PC Settings app instead of the fly-out</li>
+			<li>Multiple annimations have been altered</li>
+		</ul>
+		<h3>Features</h3>
+		<ul>
+			<li>Notifications are now added to the Action Center</li>
+			<li>Build-in mkv-file support</li>
+			<li>Build-in hvec-file support</li>
+		</ul>
+		<h3>Touch</h3>
+		<ul>
+			<li>Touching a textbox in the desktop will now auto start the onscreen keyboard</li>
+			<li>Swiping down from the screens edge will show an apps titlebar</li>
+		</ul>
+		<h3>WinRT</h3>
+		<ul>
+			<li>Apps can now run on a 1024x600 resolution</li>
+		</ul>
+		<h3>Build-in apps</h3>
+		<ul>
+			<li>PC Settings
+			  <ul>
+				<li>DataSense is added to manage data connections</li>
+				<li>Battery Saver is added to manage energy consumption on mobile devices</li>
+				<li>You can now choose if you want new Windows builds as soon as they are released, or want to wait a couple of days</li>
+			  </ul>
+			</li>
+			<li>zPC Settings has been added as a new default app (changes are compared to PC Settings)
+			  <ul>
+				<li>Multiple sections have been renamed, replaced, merged, or split</li>
+				<li>You can now add Optional Features through the app</li>
+				<li>Battery Saver (Devices) has been added to show usage details</li>
+				<li>Personalization settings have been added</li>
+				<li>Closed captioning can now be set in the app</li>
+				<li>Multiple privacy settings have been added</li>
+				<li>There are applets, through empty, for following items (sorted by section)
+				  <ul>
+					<li>System: Windows Defender, Battery saver (System), Speech, Storage sense                                </li>
+					<li>Network and WiFi: Mobile hotspot, Data Sense, VPN, DirectAccess, Ethernet, Mobile broadband</li>
+					<li>Personalization: Themes, Background, Colors, Sounds</li>
+					<li>OEM</li>
+				  </ul>
+				</li>
+			  </ul>
+			</li>
+		</ul>
+	</div>
+	<div class="panel-footer">
+		Download size: 2,00 GB (32bit) or 2,74 GB (64bit)
+	</div>
+</div>
