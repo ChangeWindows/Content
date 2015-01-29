10c4394cfa594ae27eaed09f88865bf3e2ef6510
Yannick <contact@studio384.be>
2015-01-29 21:42:16 +0100

    Add build data NEXT

---
+++ next.php
@@ -0,0 +1,140 @@
+<div class="panel panel-warning">
+	<div class="panel-heading">
+		<h3 class="panel-title">What's next?</h3>
+	</div>
+	<div class="panel-body">
+		<h2>What's next in Windows?</h2>
+		<div class="alert alert-info">
+			<h4>Future builds</h4>
+			The features listened below are from unknown builds announced during "The Next Chapter". We'll sort out which feature belongs to which build as soon as we know for sure!
+		</div>
+		<h3>Features</h3>
+		<ul>
+			<li>The virtual keyboard has been enhanced</li>
+		</ul>
+		<h3>WinRT</h3>
+		<ul>
+			<li>WinRT apps now also work on Xbox One</li>
+		</ul>
+		<h3>Start</h3>
+		<ul>
+			<li>The startmenu can be resized</li>
+			<li>A transparent option is available</li>
+		</ul>
+		<h3>Internet Explorer</h3>
+		<ul>
+			<li>IE will be replaced by Project 'Spartan' as the default browser</li>
+			<li>Improvements to the Edge render engine
+				<ul>
+					<li>Dynamic Adaptive Streaming over HTTP (DASH)</li>
+					<li>CSS
+						<ul>
+							<li>Conditional Rules</li>
+							<li>Cross-Domain Font Loading</li>
+							<li>Filters</li>
+						</ul>
+					</li>
+					<li>User input
+						<ul>
+							<li>Date related input types</li>
+							<li>Touch Events</li>
+						</ul>
+					</li>
+					<li>JavaScript
+						<ul>
+							<li>Default parameter</li>
+							<li>Generators</li>
+							<li>RegExp Build-ins (ES6)</li>
+						</ul>
+					</li>
+					<li>Realtime / Communication
+						<ul>
+							<li>WebRTC - Object RTC API</li>
+						</ul>
+					</li>
+					<li>Multimedia
+						<ul>
+							<li>Media Capture and Streams</li>
+							<li>Motion JPEG (MJPG)</li>
+							<li>Web Audio API</li>
+						</ul>
+					</li>
+					<li>Misc
+						<ul>
+							<li>&lt;img srcset&gt;</li>
+							<li>&lt;main&gt; element							
+							<li>Pointer Lock (Mouse Lock)
+						</ul>
+					</li>
+					<li>Security
+						<ul>
+							<li>Meta Referrer</li>
+						</ul>
+					</li>
+					<li>Graphics
+						<ul>
+							<li>SVG foreignobject element</li>
+						</ul>
+					</li>
+				</ul>
+			</li>
+		</ul>
+		<h3>Project 'Spartan'</h3>
+		<ul>
+			<li>Project 'Spartan' is a new browser to replace Internet Explorer as the default browser</li>
+			<li>Edge replaces Trident as a render engine</li>
+			<li>You can now draw on webpages</li>
+			<li>Comments can now be added to webpages (like Office)</li>
+			<li>Reading Mode is now build-in for desktops</li>
+			<li>Reading List is now build-in</li>
+			<li>Cortana is now build-in</li>
+		</ul>
+		<h3>Apps</h3>
+		<ul>
+			<li>Outlook
+				<ul>
+					<li>Mail has been replaced by Outlook</li>
+					<li>Outlook now has a ribbon design</li>
+				</ul>
+			</li>
+			<li>Outlook Calendar
+				<ul>
+					<li>Calendar has been replaced by Outlook Calendar</li>
+				</ul>
+			</li>
+			<li>People
+				<ul>
+					<li>People has been revamped</li>
+				</ul>
+			</li>
+			<li>Photos
+				<ul>
+					<li>Photos now can creates albums</li>
+				</ul>
+			</li>
+			<li>Settings
+				<ul>
+					<li>Windows Update
+						<ul>
+							<li>Optional drvier updates will no longer get installed automatically</li>
+							<li>Option to scan against WSUS server or Windows/Microsoft Update</li>
+							<li>Notifications when new updates are available</li>
+							<li>When setting up a device, the first update scan will show all updates and require only one restart, instead of multiple restarts and multiple series of updates</li>
+							<li>Technical Preview updates will be completely integrated in Windows Update</li>
+						</ul>
+					</li>
+				</ul>
+			</li>
+			<li>Xbox Music
+				<ul>
+					<li>Xbox Music has been revamped</li>
+					<li>You'll be able to sync playlists through OneDrive</li>
+				</ul>
+			</li>
+		</ul>
+		<h3>And further</h3>
+		<ul>
+			<li>DirectX 12 is now supported</li>
+		</ul>
+	</div>
+</div>
