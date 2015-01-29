4ee0a80c4becfcd50ba59aee009365d5c488af4d
Yannick <contact@studio384.be>
2015-01-29 21:41:51 +0100

    Add build data 9841

---
+++ 9841.php
@@ -0,0 +1,85 @@
+<div class="panel panel-warning">
+	<div class="panel-heading">
+		<h3 class="panel-title">Technical Preview v6.4.9841<span class="pull-right">01.10.'14</span></h3>
+	</div>
+	<div class="panel-body">
+		<h2>What's new in the Windows Technical Preview?</h2>
+		<p>Windows will update from version 6.0.6002 if you're on Windows Vista, 6.1.7601 if you're on Windows 7, 6.2.9200 if you're on Windows 8 or 6.3.9600 if you're on Windows 8.1 to 6.4.9841 after launching this update.</p>
+		<h3>Desktop</h3>
+		<ul>
+		  <li>Task view has been added</li>
+		  <li>Suppport for multiple desktops</li>
+		  <li>Task view replaced the App Switcher</li>
+		  <li>Modern UI apps now can run on the desktop</li>
+		  <li>Aero Snap has been updated</li>
+		  <ul>
+			<li>You can now snap up to 4 windows</li>
+			<li>When you snap a window and resize it, then snap another window to the other side, it will fill up the left screen instead of half the screen</li>
+			<li>When snapping an app, Windows will prompt a overview of other running apps to snap along</li>
+		  </ul>
+		  <li>Universal Apps now can create a shortcut on the desktop or any ohter folder</li>
+		  <li>You can now pin the Recycle Bin to the taskbar</li>
+		  <li>You can now scroll a windows by pointing the cursor to it</li>
+		  <li>When used on a touch enabled device, the icons in the notification area have more space</li>
+	  </ul>
+		<h3>Start</h3>
+		<ul>
+		  <li>The start menu has been added with support for Live Tiles</li>
+		  <li>An option to switch back to the start screen has been added under &quot;Start Menu&quot; in &quot;Taskbar and Start Menu Properties&quot;</li>
+		  <li>The Charmsbar can't be accessed with the mouse anymore, instead, a new menu button has to be clicked (keyboard and touch still available)</li>
+	  </ul>
+		<h3>File Explorer</h3>
+		<ul>
+		  <li>Home is the new default location</li>
+		  <li>An &quot;Add to favorites&quot; button has been added under &quot;Start&quot; in the ribbon</li>
+		  <li>An &quot;Share&quot; button has been added under &quot;Share&quot; in the ribbon
+			<ul>
+			  <li>You can now share files from within the File Explorer with compatible Universal Apps</li>
+			</ul>
+		  </li>
+	  </ul>
+		<h3>Internet Explorer</h3>
+		<ul>
+		  <li>Support for HTTP/2 has been added</li>
+		  <li>Improved performance for the Chakra Javascript-engine</li>
+		  <li>The Immersive Internet Explorer has been removed</li>
+		  <li>Public Suffix List is now used for interoperable Top Level Domain parsing</li>
+	  </ul>
+		<h3>Build-in apps</h3>
+		<ul>
+		  <li>Default apps have been updated to more recent versions</li>
+		  <li>PC Settings
+			<ul>
+			  <li>It's now possible to update to new Preview Builds with one click</li>
+			  <li>You can now add Active Directory accounts</li>
+			</ul>
+		  </li>
+		  <li>Search
+			<ul>
+			  <li>De Search-apps is now a default pinned item on the taskbar and can't be removed</li>
+			  <li>Search now uses a new interface, similar to Bings search results</li>
+			</ul>
+		  </li>
+		</ul>
+		<h3>And futher</h3>
+		<ul>
+		  <li>Some icons have been replaced</li>
+		  <li>The virtual keyboard now shows AutoComplete suggestions</li>
+		  <li>Command prompt
+			<ul>
+			  <li>Standarized key shortcuts</li>
+			  <li>Selecting text is now possible</li>
+			  <li>Past from clipboard is now possible</li>
+			  <li>Text wrap can be altered on resize</li>
+			  <li>Leading zeros can now be trimmed in selections</li>
+			  <li>You can alter the opacity of the window between 30 and 100 procent</li>
+			</ul>
+		  </li>
+		  <li>Preview Updates can be rolled back in the EUFI screen</li>
+		  <li>Compatibility mode for Windows 95, 98, Me, XP SP2 and XP SP3 has been removed</li>
+	  </ul>
+	</div>
+	<div class="panel-footer">
+		Download size: 2,93 GB (32bit) or 3,81 GB (64bit)<span class="pull-right"><a class="btn btn-warning" href="http://windows.microsoft.com/en-us/windows/preview-iso">Get the update now</a></span>
+	</div>
+</div>
