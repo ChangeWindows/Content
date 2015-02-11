73b59c1b61e7dfc410b0bab717474a53a9b091ac
Kamil <devloz1996@gmail.com>
2015-02-11 21:37:15 +0100

    Some fixes
    First time to commit. Question by the way: Is it EN-US or EN-GB page?

--- 9926.php
+++ 9926.php
@@ -12,15 +12,15 @@
 		<h3>Desktop</h3>
 		<ul>
 			<li>The desktop has received a new interface</li>
-			<li>Multiple annimations have been altered</li>
+			<li>Some animations have been altered</li>
 			<li>Win32 apps now also use gestures</li>
-			<li>Multiple icons have been redesigned to match the new UI</li>
+			<li>Some icons have been redesigned to match the new UI</li>
 			<li>The search button is replaced with a searchbar by default</li>
 			<li>You can now choose the default folder for File Explorer</li>
 			<li>Win32 window chromes are now more similar to WinRT apps</li>
 			<li>Personalisation
 				<ul>
-					<li>The window colorchoises have been changed</li>
+					<li>The window color choices have been changed</li><!-- Ambiguous, what's this? -->
 					<li>There are new backgrounds</li>
 				</ul>
 			</li>
@@ -29,7 +29,7 @@
 		<ul>
 			<li>Continuum is now available</li>
 			<li>The Start grid has been improved</li>
-			<li>The Start screen grid now scrolls vertical</li>
+			<li>The Start screen grid now scrolls vertically</li>
 			<li>Tiles no longer use a gradient and border</li>
 		</ul>
 		<h3>Features</h3>
@@ -39,19 +39,19 @@
 					<li>The Action Center has been revamped</li>
 					<li>You can now change quick settings in the Action Center</li>
 					<li>Notifications now can contain actions</li>
-					<li>A swype from the right now opens the Action Center</li>
+					<li>A swipe from the right now opens the Action Center</li>
 					<li>Quick settings can now contain more settings</li>
 					<li>Notifications have received a new design</li>
 				</ul>
 			</li>
 			<li>It's easier to connect to wireless audio and video devices</li>
 			<li>ALT+TAB now opens a view that's merged with the Task View</li>
-			<li>Calculator has been removed</li>
+			<li>Classic Calculator has been removed</li>
 			<li>A "Pin to quick access" options has been added to the ribbon in the File Explorer, replacing Favorites</li>
 		</ul>
 		<h3>Internet Explorer</h3>
 		<ul>
-			<li>The Edge 12 rendering engine has been updated with lots of new features
+			<li>The Edge 12 rendering engine
 				<ul>
 					<li>HTTP Strict Transport Security (HSTS)</li>
 					<li>HTTP Live Streaming (HLS)</li>
@@ -61,7 +61,7 @@
 					<li>Improved ECMAScript non-standard support</li>
 				</ul>
 			</li>
-			<li>F12 developertools improvements
+			<li>F12 developer tools
 				<ul>
 					<li>New and improved Network Tools</li>
 					<li>HTML and CSS Pretty Printing support</li>
@@ -78,7 +78,7 @@
 			<li>Full screen mode is now a button on the window instead of in the hamburger menu</li>
 			<li>Settings fly-out now appears within an app</li>
 			<li>The Charms bar has been removed</li>
-			<li>Apps can now run within File Explorer (ot pick a photo, Photos will load in File Explorer, for example)</li>
+			<li>Apps can now run within File Explorer (or pick a photo, Photos will load in File Explorer, for example)</li>
 		</ul>
 		<h3>Cortana</h3>
 		<ul>
@@ -126,7 +126,7 @@
 			<li>OneNote
 				<ul>
 					<li>OneNote has been updated to OneNote Preview</li>
-					<li>OneNote now has a ribon design</li>
+					<li>OneNote now has a ribbon design</li>
 					<li>You can now give feedback from the sidebar</li>
 					<li>You can now sync text-only for slow or mobile data connections</li>
 					<li>The radial menu has been removed</li>
@@ -145,8 +145,8 @@
 				<ul>
 					<li>Photos has been revamped</li>
 					<li>New Photos manage settings and features</li>
-					<li>Photos now remove duplicates automaticaly</li>
-					<li>Photos are now enhanced automaticaly</li>
+					<li>Photos now remove duplicates automatically</li>
+					<li>Photos are now enhanced automatically</li>
 				</ul>
 			</li>
 			<li>Settings
@@ -155,7 +155,7 @@
 					<li>Settings has been revamped completely</li>
 					<li>New options for Aero Snap have been added</li>
 					<li>When downloading a new build, Windows Update will also show a progress bar</li>
-					<li>You can now choose to enable scrolling in unactive windows</li>
+					<li>You can now choose to enable scrolling in inactive windows</li>
 					<li>There are new options to schedule restarts</li>
 					<li>Universal scroll is now added as an option under settings</li>
 				</ul>
@@ -178,12 +178,11 @@
 		</ul>
 		<h3>Media support</h3>
 		<ul>
-			<li>System-wide support for the FLAC audio format</li>
-			<li>System-wide support for the ALAC audio format</li>
+			<li>System-wide support for the FLAC & ALAC audio formats</li>
 		</ul>
 		<h3>And further</h3>
 		<ul>
-			<li>The Windows kernel has been updated from version 6.4 to 10.0</li>
+			<li>The Windows kernel major version number changed from version 6.4 to 10.0</li>
 			<li>The builds are now called Windows 10 Pro Technical Preview (instead of Windows Technical Preview)</li>
 			<li>The installation interface has been altered</li>
 			<li>The preview is now available in Japanese, Russian, German, French, French (Canada), Korean, Italian, Spanish, Spanish (Latin America), Traditional Chinese, Swedish, Finnish, Turkish, Arabic, Dutch, Czech, Polish, Thai, Vietnamese (Language Interface Pack), Catalan (Language Interface Pack), and Hindi (Language Interface Pack)</li>
