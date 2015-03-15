12e216d63cdc13b28131bae1b5b61815b2fafc9a
Yannick <contact@studio384.be>
2015-03-15 10:02:13 +0100

    Add information about build 10036

---
+++ 10036.php
@@ -0,0 +1,99 @@
+<div class="panel panel-danger">
+	<div class="panel-heading">
+		<h3 class="panel-title">Technical Preview v10.0.10036<span class="pull-right">14.03.'15</span></h3>
+	</div>
+	<div class="panel-body">
+		<h2>What's new in the Windows 10 Technical Preview build 10036?</h2>
+		<div class="alert alert-danger">
+			<h4>Leaked build</h4>
+			This build leaked onto the internet, we won't concider this build any further.
+		</div>
+		<h3>Desktop</h3>
+		<ul>
+			<li>You can now drag windows to another desktop in the Task View</li>
+			<li>Task View has been updated with a new interface</li>
+			<li>You can now choose to display all active apps from all desktops in the taskbar, or only the apps that are on the current desktop</li>
+			<li>The start button now has a smaller Windows logo</li>
+			<li>When clicking on the network icon, a new window shows up to manage your connection without opening Settings</li>
+			<li>The window preview in the taskbar has been updated with a new design</li>
+			<li>Multiple icons have been updated</li>
+			<li>When the taskbar is small, the date will still be visible</li>
+		</ul>
+		<h3>Start</h3>
+		<ul>
+			<li>The start menu now can be made transparent</li>
+			<li>&quot;Places&quot; no longer have icons, and the &quot;Places&quot; title has been removed</li>
+			<li>The &quot;All apps&quot; icon has been changed</li>
+			<li>You can now put the PC to sleep with the power button in start</li>
+			<li>Folders now have an arrow to indicate that they contain a dropdown</li>
+			<li>Internet Explorer isn't listed in the &quot;All apps&quot; list</li>
+		</ul>
+		<h3>Internet Explorer</h3>
+		<ul>
+			<li>Internet Explorer has been updated to version 11.0.10011</li>
+			<li>Improved support for ECMAScript 6</li>
+			<li>Improved support for CSS3 standards</li>
+			<li>Improved support for HTML5 standards</li>
+			<li>New flags have been added to about:flags</li>
+		</ul>
+		<h3>Features</h3>
+		<ul>
+			<li>The lock screen now shows icons in the bottom right corner, instead of under the clock</li>
+			<li>The login screen has been redesigned</li>
+			<li>You can now print to PDF natively</li>
+		</ul>
+		<h3>Installation</h3>
+		<ul>
+			<li>The installation UI has been updated</li>
+			<li>The installation now shows a percentage that is a total of the full installation, rather then a percentage for each step</li>
+			<li>Cortana now gets introduced during installation</li>
+			<li>It's now more clear hwo to maek a new Microsoft Account, or how to use a local account</li>
+		</ul>
+		<h3>Apps</h3>
+		<ul>
+			<li>Reading List
+				<ul>
+					<li>Reading List now notifies you of its Windows Phone counterpart</li>
+				</ul>
+			</li>
+			<li>Windows Store beta
+				<ul>
+					<li>Small changes to the UI in general</li>
+				</ul>
+			</li>
+			<li>OneNote
+				<ul>
+					<li>OneNote Preview is now the default version and no longer requires an update to be installed to show up</li>
+				</ul>
+			</li>
+			<li>Settings</li>
+			<ul>
+				<li>There are new app management tools</li>
+				<li>Regular Win32 apps are now also displayed in with the WinRT apps in the Settings</li>
+				<li>UI items have been refreshed</li>
+				<li>You can now choose to receive updates through P2P</li>
+				<li>Windows Defender settings have been added</li>
+				<li>You can now choose the behavior of the Tablet mode switch</li>
+			</ul>
+			<li>Windows Feedback
+				<ul>
+					<li>The Feedback app now has an improved interface</li>
+				</ul>
+			</li>
+			<li>Insider Hub
+				<ul>
+					<li>You can now track the feedback you've given</li>
+				</ul>
+			</li>
+			<li>Scan
+				<ul>
+					<li>The Scan app has been revamped</li>
+				</ul>
+			</li>
+		</ul>
+		<h3>And further</h3>
+		<ul>
+			<li>Loading screens for Restarting, Shutting down, etc. have been redesigned</li>
+		</ul>
+	</div>
+</div>
