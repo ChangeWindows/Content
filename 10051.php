856b9c907b9332615d5b3c1cee336f658292a025
Yannick <contact@studio384.be>
2015-04-29 16:31:34 +0200

    Add 10051 - Leaked build

---
+++ 10051.php
@@ -0,0 +1,54 @@
+<div class="panel panel-danger">
+	<div class="panel-heading">
+		<h3 class="panel-title">Technical Preview v10.0.10051<span class="pull-right">05.04.'15</span></h3>
+	</div>
+	<div class="panel-body">
+		<h2>What's new in the Windows 10 Technical Preview build 10051?</h2>
+		<div class="alert alert-danger">
+			<h4>Leaked build</h4>
+			This build leaked onto the internet, we won't concider this build any further.
+		</div>
+		<h3>Internet Explorer</h3>
+        <ul>
+        	<li>Major improvements to ECMAScript 6 support (from 73% to 82%)
+        		<ul>
+        			<li>Default function parameters</li>
+        			<li>Rest parameters</li>
+        			<li>Spread (...) operator</li>
+        			<li>RegEx &quot;y&quot; and &quot;u&quot; flags</li>
+        			<li>Generators</li>
+        			<li>Proxy</li>
+        			<li>Function &quot;name&quot; property</li>
+        			<li>String.prototype methods</li>
+       			</ul>
+        	</li>
+       	</ul>
+		<h3>"Project Spartan"</h3>
+        <ul>
+        	<li>Project Spartan has been updated from version 0.10 to 0.11</li>
+        	<li>You can now find your downloads in the download manager</li>
+        	<li>You can now open Internet Explorer from within Spartan</li>
+        	<li>The &quot;Add to&quot; dialogue has been improved</li>
+        </ul>
+		<h3>Apps</h3>
+		<ul>
+			<li>Health &amp; Fitness is no longer pinned to start by default</li>
+			<li>Microsoft Family
+				<ul>
+					<li>Microsoft Family has been added as an app</li>
+				</ul>
+			</li>
+			<li>Outlook
+				<ul>
+					<li>Mail has been replaced by Outlook</li>
+					<li>Outlook now has a ribbon design</li>
+				</ul>
+			</li>
+			<li>Outlook Calendar
+				<ul>
+					<li>Calendar has been replaced by Outlook Calendar</li>
+				</ul>
+			</li>
+		</ul>
+	</div>
+</div>
