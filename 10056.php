710f610a0b278b7c59a06379e4e08907a5948645
Yannick <contact@studio384.be>
2015-04-29 16:31:38 +0200

    Add 10056 - Leaked build

---
+++ 10056.php
@@ -0,0 +1,82 @@
+<div class="panel panel-danger">
+	<div class="panel-heading">
+		<h3 class="panel-title">Technical Preview v10.0.10056<span class="pull-right">10.04.'15</span></h3>
+	</div>
+	<div class="panel-body">
+		<h2>What's new in the Windows 10 Technical Preview build 10056?</h2>
+		<div class="alert alert-danger">
+			<h4>Leaked build</h4>
+			This build leaked onto the internet, we won't concider this build any further.
+			The list described below is in comparision with build 10051.
+		</div>
+		<h3>Start</h3>
+        <ul>
+        	<li>The Power button is now showed above the All apps button</li>
+        	<li>There are some new icons</li>
+        	<li>It is now possible to resize the start menu</li>
+        	<li>There are now 3 collumns per group instead of 4</li>
+        	<li>You can now resize start</li>
+        	<li>Tiles for Win32 apps no longer take a color based on the icon of that tile, but follows the window color</li>
+        	<li>Transparancy can be disabled</li>
+        	<li>Recently installed apps are now displayed on the top of the all apps list</li>
+       	</ul>
+		<h3>"Project Spartan"</h3>
+        <ul>
+        	<li>You can now save PDFs you opened in the browser</li>
+        	<li>Spartan now has an about:flags page with new options compared to Internet Explorer</li>
+        </ul>
+		<h3>Desktop</h3>
+        <ul>
+        	<li>The taskbar is now transparent</li>
+        	<li>The TaskView button has a new icon</li>
+        	<li>The search bar is now the full hight of the taskbar</li>
+        	<li>Some icons have been updated</li>
+        	<li>The taskbar is now dark by default</li>
+        	<li>You can now enable and disable the color of the taskbar and startscreen</li>
+        	<li>There is a new calendar fly-out for the system tray</li>
+        	<li>The notification center now takes on the color you've choosen for windows and the taskbar</li>
+        	<li>Windows no longer take on a color and are now always gray</li>
+       	</ul>
+		<h3>WinRT</h3>
+        <ul>
+        	<li>Apps no longer have a restricted minimum size</li>
+       	</ul>
+		<h3>Apps</h3>
+		<ul>
+			<li>Settings
+				<ul>
+					<li>The app has received a minor design update</li>
+					<li>Multiple new personalization settings have been added</li>
+				</ul>
+			</li>
+			<li>Weather
+				<ul>
+					<li>The Weather tile has a new color</li>
+					<li>Weather has received a new UI</li>
+				</ul>
+			</li>
+			<li>Sports
+				<ul>
+					<li>Sports has received a new UI</li>
+				</ul>
+			</li>
+			<li>Money
+				<ul>
+					<li>Money has received a new UI</li>
+				</ul>
+			</li>
+			<li>News
+				<ul>
+					<li>News has received a new UI</li>
+					<li>The News app now has a black tile and blue interface</li>
+				</ul>
+			</li>
+			<li>Microsoft Solitaire Collection Preview
+				<ul>
+					<li>Microsoft Solitaire Collection Preview is now a default app</li>
+					<li>The app has, compared to its stable counterpart, a brand new UI</li>
+				</ul>
+			</li>
+		</ul>
+	</div>
+</div>
