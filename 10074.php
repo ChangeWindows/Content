97c316c2d8d33fd0d35641450481d3090f0bea3c
Yannick <contact@studio384.be>
2015-05-20 16:52:25 +0200

    Update build 10074, isn't a leak anymore...

--- 10074.php
+++ 10074.php
@@ -1,13 +1,9 @@
-<div class="panel panel-danger">
+<div class="panel panel-warning">
 	<div class="panel-heading">
-		<h3 class="panel-title">Insider Preview v10.0.10074<span class="pull-right">TBA</span></h3>
+		<h3 class="panel-title">Insider Preview v10.0.10074<span class="pull-right">29.04.'15</span></h3>
 	</div>
 	<div class="panel-body">
 		<h2>What's new in the Windows 10 Insider Preview build 10074?</h2>
-		<div class="alert alert-danger">
-			<h4>Leaked build</h4>
-			This build leaked onto the internet, we won't concider this build any further.
-		</div>
 		<p>Windows will update from version 10.0.10061 to 10.0.10074 after launching this update.</p>
 		<h3>Start and search</h3>
         <ul>
