1081dbc6915489ea07dbb2e415c1fa9e0e63a0fe
Yannick <contact@studio384.be>
2015-05-20 16:52:53 +0200

    Revamp next

--- next.php
+++ next.php
@@ -1,12 +1,62 @@
 <div class="panel panel-warning">
 	<div class="panel-heading">
-		<h3 class="panel-title">What's next?</h3>
+		<h3 class="panel-title">Next up</h3>
 	</div>
 	<div class="panel-body">
-		<h2>What's next in Windows?</h2>
+		<h2>What's next in the Windows Insider Preview?</h2>
 		<div class="alert alert-info">
 			<h4>Future builds</h4>
-			The features listened below are from unknown builds announced during "The Next Chapter" and other reliable sources. We'll sort out which feature belongs to which build as soon as we know for sure!
+			The features listed below are confirmed by leaks to exist in the next Insider Preview.
+		</div>
+		<h3>Start</h3>
+		<ul>
+			<li>The "Expand start" button has been removed</li>
+			<li>Support for jumplists</li>
+			<li>&quot;Places&quot; has been removed</li>
+			<li>File Explorer and Settings are now shown above Power options</li>
+			<li>Start will now give a suggestion for new apps</li>
+			<li>Tablet improvements
+				<ul>
+					<li>The apps list is no longer displayed onscreen by default</li>
+					<li>Tiles are displayed larger</li>
+					<li>There is now a global back button</li>
+				</ul>
+			</li>
+			<li>Tiles can now follow the user's accent color</li>
+		</ul>
+		<h3>Interface</h3>
+		<ul>
+			<li>Apps will now display the back-button int the titlebar when required, tablets will show this button in the taskbar</li>
+			<li>Switch-buttons and sliders have received a new design</li>
+			<li>The window border chrome has been updated</li>
+		</ul>
+		<h3>Features</h3>
+		<ul>
+			<li>Backup &amp; restore has been readded from Windows 7</li>
+			<li>&quot;Play to&quot; has been renamed &quot;Cast to Device&quot;</li>
+			<li>In China, users can now choose between Cortana and Huna</li>
+			<li>Cortana now has a Feedback button</li>
+		</ul>
+		<h3>Edge</h3>
+		<ul>
+			<li>Project Spartan has been renamed Edge and has a new icon</li>
+			<li>There is a new Internet Explorer-like New tabs page</li>
+			<li>There is a new MSN.com-like New tabs page</li>
+			<li>Edge can now save passwords and forms</li>
+			<li>Edge now supports InPrivate browsing</li>
+			<li>Improvements to the Edge render engine</li>
+		</ul>
+	</div>
+</div>
+<div class="panel panel-warning">
+	<div class="panel-heading">
+		<h3 class="panel-title">Next up</h3>
+	</div>
+	<div class="panel-body">
+		<h2>What's next for Windows?</h2>
+		<div class="alert alert-info">
+			<h4>Future builds</h4>
+			The features listed below are confirmed to be in a future version of Windows, but we are not sure when they will be available. Further, some of these features might land together with the features listed above, but simply aren't confirmed.
 		</div>
 		<h3>Features</h3>
 		<ul>
@@ -22,14 +72,23 @@
 			<li>Build in ssa-subtitle support</li>
 			<li>Build in ass-subtitle support</li>
 			<li>Build in srt-subtitle support</li>
+			<li>Lock screen: Windows Spotlight
+				<ul>
+					<li>The Lock screen will provide information about new features in Windows 10</li>
+					<li>The Lock screen will provide information about apps</li>
+				</ul>
+			</li>
 		</ul>
 		<h3>Internet Explorer</h3>
 		<ul>
 			<li>The Edge render engine will be removed from Internet Explorer</li>
 			<li>IE will be reset back to version 11.0.9600</li>
 		</ul>
-		<h3>&quot;Project Spartan&quot;</h3>
+		<h3>Edge</h3>
 		<ul>
+			<li>Project Spartan has been renamed Edge and has a new icon</li>
+			<li>Extension support</li>
+			<li>More Cortana scenarios</li>
 			<li>Improvements to the Edge render engine
 				<ul>
 					<li>Dynamic Adaptive Streaming over HTTP (DASH)</li>
@@ -48,11 +107,6 @@
 							<li>SIMD (ES7)</li>
 						</ul>
 					</li>
-					<li>Realtime / Communication
-						<ul>
-							<li>WebRTC - Object RTC API</li>
-						</ul>
-					</li>
 					<li>Multimedia
 						<ul>
 							<li>Motion JPEG (MJPG)</li>
@@ -60,9 +114,14 @@
 					</li>
 					<li>Misc
 						<ul>
-						<li>ARIA role 'none'
-							<li>HTML Sections in Accessibility API
-							<li>Pointer Lock (Mouse Lock)							
+							<li>ARIA role 'none'</li>
+							<li>HTML Sections in Accessibility API</li>		
+							<li>Pointer Lock (Mouse Lock)</li>			
+						</ul>
+					</li>
+					<li>Realtime / Communication
+						<ul>
+							<li>WebRTC - Object RTC API</li>
 						</ul>
 					</li>
 					<li>Security
