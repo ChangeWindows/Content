5e24338274fd1deec7c39915ee31963b46f4eb27
Yannick <contact@studio384.be>
2015-05-20 16:53:00 +0200

    Add build 10122

---
+++ 10122.php
@@ -0,0 +1,47 @@
+<div class="panel panel-warning">
+	<div class="panel-heading">
+		<h3 class="panel-title">Insider Preview v10.0.10122 &middot; Developing<span class="pull-right">20.05.'15</span></h3>
+	</div>
+	<div class="panel-body">
+		<h2>What's new in the Windows 10 Insider Preview build 10122? &middot; Developing</h2>
+		<p>Windows will update from version 10.0.10074 to 10.0.10122 after launching this update.</p>
+		<h3>Start</h3>
+		<ul>
+			<li>The "Expand start" button has been removed</li>
+			<li>Support for jumplists</li>
+			<li>&quot;Places&quot; has been removed</li>
+			<li>File Explorer and Settings are now shown above Power options</li>
+			<li>Start will now give a suggestion for new apps</li>
+			<li>Tablet improvements
+				<ul>
+					<li>The apps list is no longer displayed onscreen by default</li>
+					<li>Tiles are displayed larger</li>
+					<li>There is now a global back button</li>
+				</ul>
+			</li>
+			<li>Tiles can now follow the user's accent color</li>
+		</ul>
+		<h3>Interface</h3>
+		<ul>
+			<li>Apps will now display the back-button int the titlebar when required, tablets will show this button in the taskbar</li>
+			<li>Switch-buttons and sliders have received a new design</li>
+			<li>The window border chrome has been updated</li>
+		</ul>
+		<h3>Features</h3>
+		<ul>
+			<li>Backup &amp; restore has been readded from Windows 7</li>
+			<li>&quot;Play to&quot; has been renamed &quot;Cast to Device&quot;</li>
+			<li>In China, users can now choose between Cortana and Huna</li>
+			<li>Cortana now has a Feedback button</li>
+		</ul>
+		<h3>Edge</h3>
+		<ul>
+			<li>Project Spartan has been renamed Edge and has a new icon</li>
+			<li>There is a new Internet Explorer-like New tabs page</li>
+			<li>There is a new MSN.com-like New tabs page</li>
+			<li>Edge can now save passwords and forms</li>
+			<li>Edge now supports InPrivate browsing</li>
+			<li>Improvements to the Edge render engine</li>
+		</ul>
+	</div>
+</div>
