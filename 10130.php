a9140e0b8908dcee9fa4a6d1f5de7011789617a8
Yannick <contact@studio384.be>
2015-05-30 01:22:46 +0200

    Add build 10130

---
+++ 10130.php
@@ -0,0 +1,57 @@
+<div class="panel panel-warning">
+	<div class="panel-heading">
+		<h3 class="panel-title">Insider Preview v10.0.10130<span class="pull-right">29.05.'15</span></h3>
+	</div>
+	<div class="panel-body">
+		<h2>What's new in the Windows 10 Insider Preview build 10130?</h2>
+		<p>Windows will update from version 10.0.10122 to 10.0.10130 after launching this update.</p>
+		<h3>Start</h3>
+		<ul>
+			<li>More apps now use the accent color as their tile color (Photos, Alarms &amp; Clocks, Maps, etc)</li>
+			<li>You can add new locations to the left side part of the menu</li>
+			<li>When new apps are available, there will be a little badge on the &quot;All apps&quot; button and hamburger menu</li>
+		</ul>
+		<h3>Interface</h3>
+		<ul>
+			<li>Some of the icons have been revamped</li>
+			<li>For virtual desktops, the taskbar will now be filtered by default</li>
+			<li>There is now an universal back button on the taskbar in tablet mode</li>
+			<li>The progress bar behind icons in the taskbar now works for every state again and has been redesigned</li>
+			<li>The clock widget in the taskbar has been redesigned</li>
+			<li>Jumplists have been redesigned</li>
+			<li>The Action Center no longer has a border</li>
+			<li>You can now swipe down from the edge of a screen to show app comands like Windows 8.1</li>
+		</ul>
+		<h3>Features</h3>
+		<ul>
+			<li>Cortana
+				<ul>
+					<li>Windows + C now opens Cortana instead of the Charms bar</li>
+				</ul>
+			</li>
+			<li>Printing to PDF is now called &quot;Microsoft print to PDF&quot;</li>
+		</ul>
+		<h3>Project Spartan</h3>
+		<ul>
+			<li>Project Spartan has been updated from version 0.13 to 0.15</li>
+			<li>Project Spartan is now capable of going full screen, where it used to go in full window</li>
+			<li>The Settings pane has been split in 2 pages</li>
+			<li>It is now possible to pin panes to the side to let them stay open while browsing</li>
+			<li>Improved print options</li>
+			<li>Improvements to address bar badges</li>
+			<li>Reading Mode now supports more screensizes and content types</li>
+		</ul>
+		<h3>Apps</h3>
+		<ul>
+			<li>Video</li>
+			<ul>
+				<li>The full screen feature now works properly</li>
+			</ul>
+			<li>Solitaire Collection Beta
+				<ul>
+					<li>Solitaire Collection Preview has been renamed to Solitaire Collection Beta</li>
+				</ul>
+			</li>
+		</ul>
+	</div>
+</div>
