1560b4863345aa9f6a6f571dfcd08def3b3bb7ad
Yannick <contact@studio384.be>
2015-06-19 22:02:40 -0700

    Add information on build 10147

---
+++ 10147.php
@@ -0,0 +1,86 @@
+<div class="panel panel-danger">
+	<div class="panel-heading">
+		<h3 class="panel-title">Insider Preview v10.0.10147<span class="pull-right">18.06.'15</span></h3>
+	</div>
+	<div class="panel-body">
+		<h2>What's new in the Windows 10 Insider Preview build 10147?</h2>
+		<?php echo $alerts; ?>
+		<p>Windows will update from version 10.0.10130 to 10.0.10147 after launching this update.</p>
+		<div class="alert alert-danger">
+			<h4>Leaked build</h4>
+		</div>
+	</div>
+	<img src="img/10147.png" class="build-img" />
+	<div class="panel-body">
+		<h3>Start</h3>
+		<ul>
+			<li>More apps now use the accent color as their tile color</li>
+			<li>The full screen start menu now shows the power and all apps button at the bottom</li>
+			<li>The hamburger button no longer shows a notification icon when new apps are installed, this is now shown at the all apps button</li>
+			<li>New animations for tiles, including the added-then-removed-now-added-again 3D-animation</li>
+			<li>New boot animations for apps</li>
+			<li>Most WinRT tiles can now take the large tile size (including Edge, Calculator, etc)</li>
+			<li>The smilies for additional icons above power have been replaced with actual indicating icons (and some have been updated)</li>
+			<li>Full screen start is now aligned higher, allowing more tiles in the height without a scrollbar appearing</li>
+		</ul>
+		<h3>Interface</h3>
+		<ul>
+			<li>The window chrome of Win32 apps is now the same as for WinRT apps</li>
+			<li>The clock, language and other notification-fly-outs have been updated with a transparent background</li>
+			<li>The back-button no longer takes the accent color in some apps</li>
+			<li>Apps no longer have to take a white icon on the taskbar anymore</li>
+			<li>More apps have a tile-less icon on the taskbar</li>
+			<li>Multiple icons have been revamped</li>
+			<li>All Win32 controls have been given a redesign</li>
+			<li>The installation experience has been updated, it also introduces more apps</li>
+			<li>There are new default wallpapers</li>
+			<li>Propertie windows have received a visual update</li>
+			<li>In tablet mode, icons no longer disappear behind the Task View, Search and Back button when app icons are shown</li>
+		</ul>
+		<h3>Features</h3>
+		<ul>
+			<li>Action center
+				<ul>
+					<li>New quick settings for Notes and Quiet hours</li>
+				</ul>
+			</li>
+			<li>You can now set a timer in the snapping tool</li>
+		</ul>
+		<h3>Edge</h3>
+		<ul>
+			<li>Project Spartan has been updated from version 15 to 19 and is now called Microsoft Edge</li>
+			<li>Edge has new icons</li>
+			<li>The feedback icon has been removed from the main bar</li>
+			<li>There is a new share icon on the url bar</li>
+			<li>You can now enable a dark theme for Edge</li>
+			<li>Improved ECMAScript 6 support (without flags)</li>
+			<li>You can now manage your passwords</li>
+			<li>You can now change the search provider</li>
+			<li>When closing Edge with 2 or more tabs open, it will now warn you</li>
+			<li>Pinned hubs now look better</li>
+			<li>You can now show a home button</li>
+			<li>Pinning sites to start is broken in that it no longer has the site's theme, but a normal Edge logo</li>
+		</ul>
+		<h3>Apps</h3>
+		<ul>
+			<li>Multiple apps have been updated</li>
+			<li>The &quot;Store&quot; apps has been removed, leaving only &quot;Store (beta)&quot;</li>
+			<li>New &quot;Get Office&quot; app</li>
+			<li>New &quot;Phone companion&quot; app</li>
+			<li>Insider Hub
+				<ul>
+					<li>The Insider Hub has to be activated in &quot;Add features&quot;</li>
+					<li>There Insider Hub has a new icon</li>
+				</ul>
+			</li>
+			<li>Settings
+				<ul>
+					<li>There are new animations when switching applets</li>
+					<li>Automatic color picker now works </li>
+					<li>You can now enable features that aren't enabled by default trough Windows Update</li>
+					<li>You can now exit the Windows Insider program</li>
+				</ul>
+			</li>
+		</ul>
+	</div>
+</div>
