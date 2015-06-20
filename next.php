2234d4b242025e32408e27f1bcdc608785ce7be8
Yannick <contact@studio384.be>
2015-06-19 22:02:52 -0700

    Update with latest information

--- next.php
+++ next.php
@@ -8,7 +8,63 @@
 			<h4>Future builds</h4>
 			The features listed below are confirmed by leaks to exist in the next Insider Preview.
 		</div>
-		<h3>No known changes for the next build</h3>
+		<h3>Start</h3>
+		<ul>
+			<li>More apps now use the accent color as their tile color</li>
+			<li>The full screen start menu now shows the power and all apps button at the bottom</li>
+			<li>The hamburger button no longer shows a notification icon when new apps are installed, this is now shown at the all apps button</li>
+			<li>New animations for tiles, including the added-then-removed-now-added-again 3D-animation</li>
+			<li>New boot animations for apps</li>
+			<li>Most WinRT tiles can now take the large tile size (including Edge, Calculator, etc)</li>
+		</ul>
+		<h3>Interface</h3>
+		<ul>
+			<li>The window chrome of Win32 apps is now the same as for WinRT apps</li>
+			<li>The clock, language and other notification-fly-outs have been updated with a transparent background</li>
+			<li>The back-button no longer takes the accent color</li>
+			<li>Apps no longer have to take a white icon on the taskbar anymore</li>
+			<li>More apps have a tile-less icon on the taskbar</li>
+			<li>Multiple icons have been revamped</li>
+			<li>All Win32 controls have been given a redesign</li>
+			<li>The installation experience has been updated, it also introduces more apps</li>
+		</ul>
+		<h3>Features</h3>
+		<ul>
+			<li>Action center
+				<ul>
+					<li>New quick settings for Notes and Quiet hours</li>
+				</ul>
+			</li>
+			<li>You can now set a timer in the snapping tool</li>
+		</ul>
+		<h3>Edge</h3>
+		<ul>
+			<li>Project Spartan has been updated from version 15 to 19 and is now called Microsoft Edge</li>
+			<li>Edge has new icons</li>
+			<li>The feedback icon has been removed from the main bar</li>
+			<li>There is a new share icon on the url bar</li>
+			<li>You can now enable a dark theme for Edge</li>
+			<li>Improved ECMAScript 6 support (without flags)</li>
+			<li>You can now change the search provider</li>
+			<li>When closing Edge with 2 or more tabs open, it will now warn you</li>
+			<li>Pinned hubs now look better</li>
+			<li>You can now show a home button</li>
+		</ul>
+		<h3>Apps</h3>
+		<ul>
+			<li>Multiple apps have been updated</li>
+			<li>The &quot;Store&quot; apps has been removed, leaving only &quot;Store (beta)&quot;</li>
+			<li>New &quot;Get Office&quot; app</li>
+			<li>New &quot;Phone companion&quot; app</li>
+			<li>Settings
+				<ul>
+					<li>There are new animations when switching applets</li>
+					<li>Automatic color picker now works </li>
+					<li>You can now enable features that aren't enabled by default trough Windows Update</li>
+					<li>You can now exit the Windows Insider program</li>
+				</ul>
+			</li>
+		</ul>
 	</div>
 </div>
 <div class="panel panel-warning">
@@ -23,7 +79,6 @@
 		</div>
 		<h3>Features</h3>
 		<ul>
-			<li>The virtual keyboard has been enhanced</li>
 			<li>Build in ssa-subtitle support</li>
 			<li>Build in ass-subtitle support</li>
 			<li>Build in srt-subtitle support</li>
@@ -33,15 +88,14 @@
 					<li>The Lock screen will provide information about apps</li>
 				</ul>
 			</li>
-		</ul>
-		<h3>Internet Explorer</h3>
-		<ul>
-			<li>The Edge render engine will be removed from Internet Explorer</li>
-			<li>IE will be reset back to version 11.0.9600</li>
+			<li>Cortana
+			  <ul>
+			    <li>Cortana will show notifications in the taskbar's search field</li>
+		      </ul>
+			</li>
 		</ul>
 		<h3>Edge</h3>
 		<ul>
-			<li>Project Spartan has been renamed Edge and has a new icon</li>
 			<li>Extension support</li>
 			<li>More Cortana scenarios</li>
 			<li>Improvements to the Edge render engine
