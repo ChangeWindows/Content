52b856f2d25212fcf0b5c601c6f277e64a22e6f2
Yannick <contact@studio384.be>
2015-07-05 15:42:25 +0200

    Add information on build 10162

---
+++ 10162.php
@@ -0,0 +1,16 @@
+<div class="panel panel-warning">
+	<div class="panel-heading">
+		<h3 class="panel-title">Desktop &middot; Insider Preview v10.0.10162<span class="pull-right">02.07.'15</span></h3>
+	</div>
+	<div class="panel-body">
+		<h2>What's new in the Windows 10 Insider Preview build 10162?</h2>
+		<?php echo $alerts; ?>
+	</div>
+	<img src="img/10162.jpg" class="build-img" />
+	<div class="panel-body">
+		<h3>Bug fixes<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
+		<ul>
+			<li>Improved reliablility, stability and battery usage</li>
+		</ul>
+	</div>
+</div>
