996912d05548088ee44f29d301cd2dd97efb9112
Yannick <contact@studio384.be>
2015-07-05 15:42:15 +0200

    Add information on build 10159

---
+++ 10159.php
@@ -0,0 +1,18 @@
+<div class="panel panel-warning">
+	<div class="panel-heading">
+		<h3 class="panel-title">Desktop &middot; Insider Preview v10.0.10159<span class="pull-right">30.06.'15</span></h3>
+	</div>
+	<div class="panel-body">
+		<h2>What's new in the Windows 10 Insider Preview build 10159?</h2>
+		<?php echo $alerts; ?>
+	</div>
+	<img src="img/10159.jpg" class="build-img" />
+	<div class="panel-body">
+		<h3>Interface<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
+		<ul>
+			<li>The Hero wallpaper is now available</li>
+			<li>The logon screen has been updated with a centered design</li>
+			<li>The Hero wallpaper is now used in the logon screen</li>
+		</ul>
+	</div>
+</div>
