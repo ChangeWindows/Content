a1c398614f234a99907f852494088117efb6fc5f
Yannick <contact@studio384.be>
2015-07-05 15:43:04 +0200

    Add information on build 10136

---
+++ 10136.php
@@ -0,0 +1,43 @@
+<div class="panel panel-warning">
+	<div class="panel-heading">
+		<h3 class="panel-title">Mobile &middot; Insider Preview v10.0.10136<span class="pull-right">16.06.'15</span></h3>
+	</div>
+	<div class="panel-body">
+		<h2>What's new in the Windows 10 Mobile Insider Preview build 10136?</h2>
+		<?php echo $alerts; ?>
+		<h3>Interface<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
+		<ul>
+			<li>There are multiple UI improvements</li>
+			<li>Pressing and holding the Windows-button will make it interface come down</li>
+			<li>The search button in "All apps" has been replaced with a search box</li>
+		</ul>
+		<h3>Features<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
+		<ul>
+			<li>Cortana
+				<ul>
+					<li>Improved Cortana interface</li>
+				</ul>
+			</li>
+		</ul>
+		<h3>Edge<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
+		<ul>
+			<li>Internet Explorer is no longer available</li>
+			<li>Full screen support</li>
+			<li>InPrivate mode is now available</li>
+		</ul>
+		<h3>Apps<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
+		<ul>
+			<li>Settings
+				<ul>
+					<li>You can now set 3G-only has highest connection speed</li>
+					<li>You now get to see a splitted interface in landscape mode on larger devices</li>
+				</ul>
+			</li>
+		</ul>
+		<h3>And further<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
+		<ul>
+			<li>Both VPN Point to Point Tunnelling Protocol (PPTP) and Secure Socket Tunnelling Protocol (SSTP) are now supported</li>
+			<li>Video stabilization</li>
+		</ul>
+	</div>
+</div>
