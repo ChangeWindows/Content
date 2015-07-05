e35be38465a3020f035b692248dd677a59c73f53
Yannick <contact@studio384.be>
2015-07-05 15:42:48 +0200

    Add information on build 10052

---
+++ 10052.php
@@ -0,0 +1,13 @@
+<div class="panel panel-warning">
+	<div class="panel-heading">
+		<h3 class="panel-title">Mobile &middot; Technical Preview v10.0.10052<span class="pull-right">21.04.'15</span></h3>
+	</div>
+	<div class="panel-body">
+		<h2>What's new in the Windows 10 Mobile Technical Preview build 10052?</h2>
+		<?php echo $alerts; ?>
+		<h3>General<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
+		<ul>
+			<li>Bug fixes</li>
+		</ul>
+	</div>
+</div>
