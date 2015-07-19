c90f97f84a8dba29d3f5bb7cc3547566ea9f446b
Yannick <contact@studio384.be>
2015-07-19 23:32:22 +0200

    Add info on build 9600u2 for desktop

---
+++ 9600u2.php
@@ -0,0 +1,58 @@
+<div class="panel panel-info">
+	<div class="panel-heading">
+		<h3 class="panel-title">Desktop &middot; Windows 8.1 Update 2 v6.3.9600.17249</h3>
+	</div>
+	<div class="panel-body">
+        <p>After Windows 8.1 Update launched in April 2014, it was tought that in the summer of that year, another update, known as Windows 8.1 Update 2 would be released. This, however, never happened and Microsoft explained that Windows 8.1 Update 2 simply didn't exist as it is spread out over multiple months (being May, June and August). However, multiple sites continued to claim Windows 8.1 Update 2 was released on August, and Windows 8.1 Update 3 in October 2014. However, as Microsoft announced Windows 8.1 Update 3 for Windows RT-devices to launch in September 2015, we can confirm to you that Windows 8.1 Update 3 didn't exist in October 2014 like many sites claim. Because of this, we do not consider Windows 8.1 Update 2 as an update that exist, however, to keep our changelog as complete as possible, we'll go over the changes in the May, June and August updates to Windows 8.1 and call these updates in general "Windows 8.1 Update 2".</p>
+		<h2>What's new in Windows 8.1 Update 2 build 9600.17107?</h2>
+		<h3>Store<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
+		<ul>
+			<li>The navigation bar is now always visible</li>
+			<li>Hitlists and Collections have been added</li>
+			<li>The start page has received a small redesign</li>
+			<li>The navigation for mouse and keyboard users has been improved</li>
+			<li>Apps now show if they are an Universel app</li>
+			<li>Promotions are now shown on an app's page when available</li>
+		</ul>
+		<h2>What's new in Windows 8.1 Update 2 build 9600.17126?</h2>
+		<h3>OneDrive<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
+		<ul>
+			<li>There is a new fly-out in the system tray</li>
+			<li>You can now force OneDrive to sync</li>
+			<li>You can now pause a sync</li>
+			<li>The fly-out now shows when was the last time OneDrive synced</li>
+			<li>There is a new option to open the OneDrive-folder in the Explorer</li>
+			<li>The dropdownmenu in the system tray has been extended with more option</li>
+			<li>There is now a link to the OneDrive-site</li>
+			<li>You can now manage your OneDrive-storage through the system tray</li>
+			<li>A link to OneDrive settings has been added</li>
+			<li>Multiple help items have been added</li>
+		</ul>
+		<h3>Internet Explorer<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
+		<ul>
+			<li>Improved support for vertical writing</li>
+		</ul>
+		<h2>What's new in Windows 8.1 Update 2 build 9600.17249?</h2>
+		<h3>PC Settings<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
+		<ul>
+			<li>Windows Update now show when it last checked on updates and when it last installed updates</li>
+			<li>You can now choose to enable or disable the touchpad when a mouse is attached</li>
+			<li>It's now possible to enable right clicks on a mousepad</li>
+			<li>Idem dito for double clicks</li>
+		</ul>
+		<h3>Internet Explorer<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
+		<ul>
+			<li>Improved support for WebGL</li>
+			<li>New updates to the F12-tools</li>
+			<li>Start of WebDriver-support</li>
+		</ul>
+		<h3>And further<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
+		<ul>
+			<li>Support for Miracast Receive</li>
+			<li>You'll now see less dialogues when using SharePoint Online</li>
+			<li>The Ruble-icon is now supported</li>
+			<li>Improved WiFi Direct APIs</li>
+			<li>New API for reading the gps-data and date recorded of an MP4-file</li>
+		</ul>
+	</div>
+</div>
