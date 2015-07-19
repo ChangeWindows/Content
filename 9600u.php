9e7bebb5f92d6465d895fb6105bc373ff6459baa
Yannick <contact@studio384.be>
2015-07-19 23:33:41 +0200

    Update build 9600 and 9600u for consistency

--- 9600u.php
+++ 9600u.php
@@ -1,67 +1,67 @@
 <div class="panel panel-info">
 	<div class="panel-heading">
-		<h3 class="panel-title">Desktop &middot; Windows 8.1 Update v6.3.9600<span class="pull-right">08.04.'14</span></h3>
+		<h3 class="panel-title">Desktop &middot; Windows 8.1 Update v6.3.9600.17031<span class="pull-right">08.04.'14</span></h3>
 	</div>
 	<div class="panel-body">
-		<h2>What's new in the Windows 8.1 Update build 9600?</h2>
+		<h2>What's new in Windows 8.1 Update build 9600.17031?</h2>
 	</div>
 	<img src="img/9600u.jpg" class="build-img" />
 	<div class="panel-body">
 		<h3>Start and All apps<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
 		<ul>
-			<li>You can now shut down Windows from the start screen (not for devices with touchscreen or smaller than 8,5 inch).</li>
-			<li>You can now search from the start screen with a visual button.</li>
-			<li>A right-click on a tile now shows a context menu.</li>
-			<li>The default start screen has been updated for new users.</li>
-			<li>The start screen now shows a notification when a new app has been installed.</li>
-			<li>You can now show more apps in the All apps view.</li>
-			<li>All apps now shows the letter of each category always.</li>
+			<li>You can now shut down Windows from the start screen (not for devices with touchscreen or smaller than 8,5 inch)</li>
+			<li>You can now search from the start screen with a visual button</li>
+			<li>A right-click on a tile now shows a context menu</li>
+			<li>The default start screen has been updated for new users</li>
+			<li>The start screen now shows a notification when a new app has been installed</li>
+			<li>You can now show more apps in the All apps view</li>
+			<li>All apps now shows the letter of each category always</li>
 		</ul>
 		<h3>Desktop<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
 		<ul>
 			<li>It's now possible to pin Windows Apps</li>
 			<li>The Windows Store is now a default pinned app</li>
-			<li>Apps now have a titlebar.</li>
-			<li>The taskbar now can be accessed within apps.</li>
-			<li>Devices without a touchscreen now boot to desktop by default.</li>
-			<li>Devices without a touchscreen now have desktop apps set as default.			</li>
+			<li>Apps now have a titlebar</li>
+			<li>The taskbar now can be accessed within apps</li>
+			<li>Devices without a touchscreen now boot to desktop by default</li>
+			<li>Devices without a touchscreen now have desktop apps set as default</li>
 		</ul>
 		<h3>Internet Explorer<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
 		<ul>
-			<li>&quot;Enterprise mode&quot; has been added to Internet Explorer.</li>
-			<li>There are new options in the F12-tools.</li>
-			<li>Improved support for ECMAScript 5.1.</li>
+			<li>&quot;Enterprise mode&quot; has been added to Internet Explorer</li>
+			<li>There are new options in the F12-tools</li>
+			<li>Improved support for ECMAScript 5.1</li>
 			<li>Improved support for WebGL</li>
 		</ul>
 		<h3>Build-in apps<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
 		<ul>
 			<li>OneDrive
 				<ul>
-					<li>SkyDrive is now OneDrive.</li>
-					<li>It's now possible to synchronise manually.</li>
-					<li>There is now a OneDrive icon in the system tray.</li>
+					<li>SkyDrive is now OneDrive</li>
+					<li>It's now possible to synchronise manually</li>
+					<li>There is now a OneDrive icon in the system tray</li>
 				</ul>
 			</li>
 			<li>Xbox Music
 				<ul>
-					<li>You can now pin artists and playlists to your start screen.</li>
-					<li>You can now use drag and drop.</li>
-					<li>Xbox Music will now show Backwards, Forwards and Play/Pause under its Aero Peak window on the taskbar.</li>
+					<li>You can now pin artists and playlists to your start screen</li>
+					<li>You can now use drag and drop</li>
+					<li>Xbox Music will now show Backwards, Forwards and Play/Pause under its Aero Peak window on the taskbar</li>
 				</ul>
 			</li>
 			<li>PC Settings
 				<ul>
-					<li>You can now manage your disk space.</li>
-					<li>You can now give your PC a new name from the app.</li>
-					<li>You can now leave a domain from the app.</li>
-					<li>A link to the Control Panel hass been added.</li>
+					<li>You can now manage your disk space</li>
+					<li>You can now give your PC a new name from the app</li>
+					<li>You can now leave a domain from the app</li>
+					<li>A link to the Control Panel hass been added</li>
 				</ul>
 			</li>
 		</ul>
 		<h3>And futher<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
 		<ul>
-			<li>You can now show more details in the Networks fly-out for a network.</li>
-			<li>Improved performance and support for WIMBoot.</li>
+			<li>You can now show more details in the Networks fly-out for a network</li>
+			<li>Improved performance and support for WIMBoot</li>
 		</ul>
 	</div>
 </div>
