ba750afab29e75b5112e6c9366ab050cdce9659d
Yannick <contact@studio384.be>
2015-07-19 23:31:44 +0200

    Add info on build 10240 for desktop

---
+++ 10240.php
@@ -0,0 +1,25 @@
+<div class="panel panel-success">
+	<div class="panel-heading">
+		<h3 class="panel-title">Desktop &middot; Windows 10 v10.0.10240.16384<span class="pull-right">15.07.'15</span></h3>
+	</div>
+	<div class="panel-body">
+		<h2>What's new in Windows 10 build 10240.16384?</h2>
+		<?php echo $alerts; ?>
+	</div>
+	<img src="img/10240.jpg" class="build-img" />
+	<div class="panel-body">
+		<h3>Microsoft Edge<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
+		<ul>
+			<li>Performance improvements</li>
+		</ul>
+		<h3>And further<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
+		<ul>
+			<li>There is no watermark anymore</li>
+			<li>First build in the production branches</li>
+		</ul>
+		<h3>Bug fixes<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
+		<ul>
+			<li>Bug fixes</li>
+		</ul>
+	</div>
+</div>
