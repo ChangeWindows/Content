6d9d9fb880de22ef6a57f5fc1ba2a63c9033e9fe
Yannick <contact@studio384.be>
2015-07-19 23:32:37 +0200

    Add info on build 9600u3 for Windows RT

---
+++ 9600u3.php
@@ -0,0 +1,8 @@
+<div class="panel panel-info">
+	<div class="panel-heading">
+		<h3 class="panel-title">Desktop &middot; Windows 8.1 Update 3 v6.3.9600</h3>
+	</div>
+	<div class="panel-body">
+        <p>Windows 8.1 Update 3 is an unreleased update for Windows 8.1 that is due for September 2015. We do not have any (reliable) information on this update except for its name and release date. Note that multiple sites are calling the October 2014 updates to Windows 8.1 "Windows 8.1 Update 3", let us make it clear that these sites are just throwing a name on something because it was rumoured back in 2013 that Windows 8.1 Update 3 would launch somewhere in late 2014. ChangeWindows will follow the official naming scheme and we'll be updating this page as soon as more information is available.</p>
+	</div>
+</div>
