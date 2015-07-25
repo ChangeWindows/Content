c28a7c1fa629380cd3971149d8dc71b27c2ea23d
Yannick <contact@studio384.be>
2015-07-26 00:16:47 +0200

    Add info on build 15116 for mobile

---
+++ 15116m.php
@@ -0,0 +1,49 @@
+<div class="panel panel-info">
+	<div class="panel-heading">
+		<h3 class="panel-title">Mobile &middot; Windows Phone 8.1 Update 2 v8.10.15116.125<span class="pull-right">10.04.'15</span></h3>
+	</div>
+	<div class="panel-body">
+		<h2>What's new in Windows Phone 8.1 build 15116.125?</h2>
+		<?php echo $alerts; ?>
+		<h3>Interface<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
+		<ul>
+			<li>De "All apps" arrow has a new design and now shows a text label</li>
+		</ul>
+		<h3>Settings<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
+		<ul>
+			<li>Settings is now devided into multiple categories</li>
+			<li>You can now search through settings</li>
+			<li>You can now pin settings to the start screen</li>
+		</ul>
+		<h3>Features<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
+		<ul>
+			<li>Bangla, Khmer, Kiswahili and Lao are now supported languages</li>
+			<li>You can now restore the start screen to its original state</li>
+			<li>Support for the MKV container</li>
+		</ul>
+		<h3>System technologies<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
+		<ul>
+			<li>Support for Voice over LTE</li>
+			<li>Support for Bluetooth Messaging Access Profile</li>
+			<li>Support for Bluetooth Human Interface Device Profile 1.1</li>
+			<li>Support for Bluetooth HID over Generic Attribute Profile</li>
+			<li>Support for Absolute volume Control</li>
+			<li>Support for VPN Configuration Service Provider</li>
+		</ul>
+		<h2>What's new in Windows Phone 8.1 build 15127.138?</h2>
+		<h3>Features<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
+		<ul>
+			<li>Anti-theft protection added</li>
+		</ul>
+		<h2>What's new in Windows Phone 8.1 build 15143.154?</h2>
+		<h3>And furhter<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
+		<ul>
+			<li>Improved support for updating to Windows 10 Mobile</li>
+		</ul>
+		<h2>What's new in Windows Phone 8.1 build 15148.160?</h2>
+		<h3>And furhter<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
+		<ul>
+			<li>Improved support for updating to Windows 10 Mobile</li>
+		</ul>
+	</div>
+</div>
