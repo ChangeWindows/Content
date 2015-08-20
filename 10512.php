c620d8f5729e40cbacc1475886b9d7baf26d7c74
Yannick <contact@studio384.be>
2015-08-20 21:35:28 +0200

    Add build 10512

---
+++ 10512.php
@@ -0,0 +1,20 @@
+<div class="panel panel-warning">
+	<div class="panel-heading">
+		<h3 class="panel-title">Mobile &middot; Insider Preview v10.0.10512<span class="pull-right">12.08.'15</span></h3>
+	</div>
+	<div class="panel-body">
+		<h2>What's new in Windows 10 Mobile Insider Preview build 10512?</h2>
+		<?php echo $alerts; ?>
+		<h3>General<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
+		<ul>
+			<li>You can now change your lock screen and background from the Photos app</li>
+			<li>The tile layout for Kids Corner has been changed</li>
+			<li>There are new wallpapers available, dated April 1975</li>
+		</ul>
+		<h3>And further<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
+		<ul>
+			<li>This is the first Windows Threshold R2 release</li>
+			<li>Bugfixes</li>
+		</ul>
+	</div>
+</div>
