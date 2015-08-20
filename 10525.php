d7d3ad119227beb2afa4d2d9ad69757d306259d9
Yannick <contact@studio384.be>
2015-08-20 21:35:39 +0200

    Add build 10525

---
+++ 10525.php
@@ -0,0 +1,29 @@
+<div class="panel panel-warning">
+	<div class="panel-heading">
+		<h3 class="panel-title">Desktop &middot; Insider Preview v10.0.10525.0<span class="pull-right">18.08.'15</span></h3>
+	</div>
+	<div class="panel-body">
+		<h2>What's new in Windows 10 Insider Preview build 10525?</h2>
+		<?php echo $alerts; ?>
+	</div>
+	<img src="img/build/10525.png" class="build-img" alt="Screenshot current build" />
+	<div class="panel-body">
+		<h3>Desktop<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
+		<ul>
+			<li>New color options</li>
+		</ul>
+		<h3>Edge<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
+		<ul>
+			<li>Base for Object RTC support</li>
+		</ul>
+		<h3>Settings<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
+		<ul>
+			<li>The Settings-icon on the taskbar has no longer a background color</li>
+			<li>The Settings-tile is now transparent for non-fresh installs too</li>
+		</ul>
+		<h3>And further<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
+		<ul>
+			<li>Improved memory management</li>
+		</ul>
+	</div>
+</div>
