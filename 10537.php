2a4e8796f1c622219c257a67174ec7b13d6b9b42
Yannick <contact@studio384.be>
2015-09-14 16:42:54 +0200

    Update info on build 10537

--- 10537.php
+++ 10537.php
@@ -5,21 +5,16 @@
 </p>
 <h2 class="ctr-warning">Windows 10 "Threshold R2" &middot; 10537</h2>
 <?php echo $alerts; ?>
-<a href="img/build/10532.png"><img src="img/build/10532.png" class="build-img" alt="Screenshot current build" /></a>
+<!--<a href="img/build/10532.png"><img src="img/build/10532.png" class="build-img" alt="Screenshot current build" /></a>-->
 <h3>Desktop<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
 <ul>
-	<li>Context menus have now a more consistent design</li>
-	<li>Improved dark and light theming</li>
+	<li>Context menus have now have a grey design instead of white</li>
 </ul>
-<h3>Edge<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
+<h3>Windows Apps<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
 <ul>
-	<li>Improved support for ECMAScript 6.0 and 7.0</li>
-	<li>Support for new input types</li>
-	<li>Support for Pointer Lock</li>
-	<li>Support for Canvas blending modes</li>
-	<li>Additional improved support for standards</li>
+	<li>Windows Apps now have an opening and closing animation</li>
 </ul>
 <h3>And further<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
 <ul>
-	<li>You can now share feedback more easly</li>
+	<li>Some icons have been replaced</li>
 </ul>
