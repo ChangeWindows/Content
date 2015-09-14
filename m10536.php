a9ccf46f7bd52fe97ca92f709eebae917ff85cd7
Yannick <contact@studio384.be>
2015-09-14 16:36:52 +0200

    Add info on build 10536

---
+++ m10536.php
@@ -0,0 +1,27 @@
+<p class="meta build-date">
+	<span class="release-version">
+		11.09.'15
+	</span>
+</p>
+<h2 class="ctr-warning">Windows 10 Mobile "Threshold R2" &middot; 10536</h2>
+<?php echo $alerts; ?>
+<p>Microsoft announced the release of build 10536 for mobile devices but cancelled the update shortly before the full roll-out began due to a bug that made it impossible to upgrade to future builds. The update was already staged and build 10536.1003 was rolled out to 200 Windows Insiders. While this is a very small roll-out, we do concider it a public release and are thus listening September 11, 2015 as this build's release date. This build was re-released as 10536.1004 on September 15, 2015..</p>
+<h2>What's new in Windows 10 Mobile build 10536.1003?</h2>
+<h3>General<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
+<ul>
+	<li>Make device visible for USB Ethernet Emulation Model</li>
+	<li>Make device show up trough USB connection and in local network</li>
+</ul>
+<h3>And further<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
+<ul>
+	<li>Mobile hotspot is now functional again</li>
+	<li>Quiet Hours\Do Not Disturb is also fixed</li>
+	<li>Pinch to zoom in maps has been fixed to work as expected</li>
+	<li>The delay for the lockscreen (to show time and data and unlock) has been fixed</li>
+</ul>
+<h2>What's new in Windows 10 Mobile build 10536.1004?</h2>
+<h3>Fixes<small><a href="http://changewindows.org"> by ChangeWindows.org</a></small></h3>
+<ul>
+	<li>Fixes a bug that caused later updates to fail</li>
+	<li>Fixes a bug that caused issues when purchasing something trough the store</li>
+</ul>
